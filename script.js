bb778628cc7bd66907b8d0d0b50bc6ac3a83ac6b
yoshinobutsuchidarecruit-lab <yoshinobutsuchidarecruit@gmail.com>
2026-01-18 15:46:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("messageBtn");
+const message = document.getElementById("message");
+
+btn.addEventListener("click", () => {
+  message.textContent = "現在、毎日少しずつ勉強しています。";
+});
+
